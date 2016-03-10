68dc5d3a251e3e94f43f3e300c15be41d88a6b1b
Jorge Murta <jmurta.jm@gmail.com>
2016-03-10 15:21:11 +0000

    Add Refresh Countdown

--- web/dev/app/main.js
+++ web/dev/app/main.js
@@ -4,9 +4,28 @@ require('./helpers/resources').init();
 new Vue({
     el: 'body',
     data: {
-        name: ''
+        name: '',
+        connected: false,
+        network: 'Votum',
+        refreshSeconds: 25,
+    },
+    methods: {
+        refreshData: function () {
+            setInterval(function () {
+                if (this.refreshSeconds == 0) {
+                    this.refreshSeconds = 25;
+
+                    // For Demo Purposes
+                    this.connected = !this.connected;
+
+                    return;
+                }
+                this.refreshSeconds = this.refreshSeconds - 1;
+            }.bind(this), 1000);
+        }
     },
     ready: function () {
         console.log('Application Ready');
+        this.refreshData();
     }
 });
